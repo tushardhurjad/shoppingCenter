5c17626a83eeeb79bf65d40b14d5ce214f8accbe
tushar dhurjad <tushard2233@gmail.com>
2023-04-15 12:47:36 +0530

    Admin Modul Setup

--- shoppingCenter/src/app/app.module.ts
+++ shoppingCenter/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminModule } from './widgets/admin/admin.module';
+import { HomeComponent } from './widgets/admin/home/home.component';
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ 
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AdminModule
   ],
   providers: [],
   bootstrap: [AppComponent]
